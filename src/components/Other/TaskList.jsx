1952451fccf7b131e040fe2f5d8ae51612e7e18e
Rahul Kumar <78557122+rahul36019@users.noreply.github.com>
2026-03-09 06:15:54 +0000

    Create dashboard

--- src/components/Other/TaskList.jsx
+++ src/components/Other/TaskList.jsx
@@ -4,14 +4,18 @@ const TaskList = () => {
   return (
     <div
       id="tasklist"
-      className=" h-full overflow-x-auto flex items-centre my-5 "
+      className="flex-1 overflow-x-auto flex gap-5 items-start my-5 w-full"
     >
-      <div className="flex-shrink-0  h-full w-[300px] bg-yellow-400 rounded-xl bg-yellow-400">
-        <div className="flex justify-between items-centre">
-          <h3 className="bg-red-600 text-sm px-3 py-1 rounded ">high</h3>
+      {/* Individual Card */}
+      <div className="flex-shrink-0 p-5 h-full w-[300px] bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">high</h3>
           <h4 className="text-sm">20 feb 2024</h4>
         </div>
-        <h2 className="mt-5 text-xl font-semibold ">Make a good video</h2>
+        <h2 className="mt-5 text-xl font-semibold">Make a good video</h2>
+        <p className="text-sm mt-2">
+          Add some description here to see how it fills the card!
+        </p>
       </div>
     </div>
   );
